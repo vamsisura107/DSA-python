0ea4194b9f00d32b2d4e7d0d6092d42f8d18a715
SURA VAMSI KRISHNA REDDY <suravamsikrishnareddy@gmail.com>
2026-04-13 10:37:06 +0530

    Update queue-List.py

--- queue-List.py
+++ queue-List.py
@@ -1,37 +1,40 @@
 def enqueue():
-    n=input("enter the element to be inserted")
+    n = input("enter the element to be inserted: ")
     queue.append(n)
-    print("  ")
+    print("Element added.")
 def dequeue():
-    if (len(queue)==0):
-        print("the queue is empty")
-        print()
+    if len(queue) == 0:
+        print("queue is empty")
     else:
-        print(queue[0],"is the elements deleted from the queue ")
+        # Fixed: Changed dequeue[0] to queue[0]
+        print(queue[0], "is the element deleted from queue")
         del queue[0]
-        print("  ")
+        print(" ")
 def display():
-    if len(queue)==0:
-        print("the queue is empty")
+    if len(queue) == 0:
+        print("list is empty")
     else:
+        print("Queue elements:", end=" ")
         for ele in queue:
-            print(ele,end=" ")
-        print("front element of the queue is ",queue[0])
-        print("rear element of the queue is ",queue[-1])
-        print()
-queue=list()
-while(1):
-    print("enter the option from below\n1-enqueue\n2-dequeue\n3-display\n4-exit")
-    str = input()
-    if str == "1":
-        print("enqueue operation")
+            print(ele, end=" ")
+        print("\nfront of the queue is:", queue[0])
+        print("rear of the queue is:", queue[-1])
+
+queue = list()
+while True:
+    print("\nenter the option from below\n1-enqueue\n2-dequeue\n3-display\n4-exit")
+    choice = input("Choice: ")
+    if choice == "1":
+        print("--- enqueue operation ---")
         enqueue()
-    elif str == "2":
-        print("dequeue operation")
+    elif choice == "2":
+        print("--- dequeue operation ---")
         dequeue()
-    elif str == "3":
-        print("display operation")
+    elif choice == "3":
+        print("--- display operation ---")
         display()
-    else:
-        print("exit from program")
+    elif choice == "4":
+        print("exit from the program")
         break
+    else:
+        print("Invalid option, please try again.")
